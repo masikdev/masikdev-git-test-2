90d493108fcf9d603f965478368f69a136483dcb
masikdev <masikroosevelt@gmail.com>
2024-01-20 20:06:23 +0700

    Menambahkan paragraph baru

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>Hello World</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur dolores aliquid libero, a praesentium ea quas facere nobis rem exercitationem suscipit, sequi fuga illum neque earum minus saepe reiciendis sit.</p>
 </body>
 </html>
